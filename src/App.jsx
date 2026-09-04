fc5f8fa9b5c8d76d0e3fd5f6a1b4ba438699a5c0
Marc Butstraen <marc.butstraen@telenet.be>
2026-09-04 10:41:56 +0200

    Refactor filtering logic and adjust input field sizes for better usability

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,6 @@ import BoekenTabel from './components/BoekenTabel'
 import Filters from './components/Filters'
 
 export default function App () {
-    {/*}
-    function jaarOpzoeken(opmerkingen) {
-        if (typeof opmerkingen !== "string") return null;
-        const m = opmerkingen.match(/\b(19|20|25)\d{2}\b/);
-        return m ? m[0] : null;   
-    }
-        */}
     
     function jaarOpzoeken(opmerkingen) {
         if (typeof opmerkingen !== "string") return null;
@@ -31,27 +24,24 @@ export default function App () {
         opmerkingen: "",
         jaartal: "",
     });
-
-
-    const gefilterd = boeken.filter(boek => {
+     
+    const gefilterd = [...boeken.filter(boek => {
         const matchNat = 
-            !filters.nationaliteit || boek.Nationaliteit.toLowerCase() === filters.nationaliteit.toLowerCase();
+            !filters.nationaliteit || boek.Nationaliteit.toLowerCase().slice(0, filters.nationaliteit.length) === filters.nationaliteit.toLowerCase();
         const matchAuteur =
-            !filters.auteurnaam || boek.Auteurnaam.toLowerCase() === filters.auteurnaam.toLowerCase();
+            !filters.auteurnaam || boek.Auteurnaam.toLowerCase().slice(0, filters.auteurnaam.length) === filters.auteurnaam.toLowerCase();
         const matchOpmerkingen =
-            !filters.opmerkingen || boek.Opmerkingen.toLowerCase() === filters.opmerkingen.toLowerCase();
+            !filters.opmerkingen || boek.Opmerkingen.toLowerCase().slice(0, filters.opmerkingen.length) === filters.opmerkingen.toLowerCase();
         const matchJaar =
             !filters.jaartal || jaarOpzoeken(boek.Opmerkingen) === filters.jaartal;
         return  matchNat && matchAuteur && matchJaar && matchOpmerkingen
-    });
-
+    })];
 
   return (
     <div>
       <VerbindingMaken onData={setBoeken} />
       <Filters AantalBoeken={boeken.length} AantalGefilterdeBoeken={gefilterd.length} filters={filters} onFilterChange = {setFilters} />
-      <BoekenTabel boeken={gefilterd} />
+      <BoekenTabel key={JSON.stringify(gefilterd)} boeken={gefilterd} />
     </div>
   );
-
 }
